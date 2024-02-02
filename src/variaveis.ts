336d55e2a5b9a6c46b6ddad1fa30a2d7d783c664
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-02-02 15:02:52 +0000

    Aprendendo variáveis

---
+++ src/variaveis.ts
@@ -0,0 +1,28 @@
+let ligado: boolean = true;
+let nome: string = "Diego";
+let idade: number = 20;
+let altura: number = 170;
+
+let nulo: null = null;
+let indefinid0: undefined = undefined;
+
+let retorno: void;
+let retornoView: any = false;
+
+let product: object = {
+    name: "Diego",
+    cidade: "jp",
+    idade: 20
+}
+
+type produtoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+}
+
+let sapato: produtoLoja = {
+    nome: "Air Jordan",
+    preco: 500.00,
+    unidades: 10
+}
