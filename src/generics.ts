97bfe9088036318000a931e03a4dce29a62e6075
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-02-08 19:44:24 +0000

    generics.ts

--- src/generics.ts
+++ src/generics.ts
@@ -1,10 +1,14 @@
-function concatArray(...itens: any[]): any[]{
+function concatArray<T>(...itens: T[]): T[]{
     return new Array().concat(itens);
 }
 
-const numArray = concatArray([1, 5], [3]);
-const stgArray = concatArray(["Diego", "Vegeta"], ["Goku"]);
-numArray.push("Saitama");
+const numArray = concatArray<number[]>([1, 5], [3]);
+
+const stgArray = concatArray<string[]>(["Diego", "Vegeta"], ["Goku"]);
+
+// Neste caso numArray foi definido acima para receber apenas números
+// com a especificação de forma dinâmica.
+// numArray.push("Saitama");
 console.log(numArray);
 console.log(stgArray);
 
